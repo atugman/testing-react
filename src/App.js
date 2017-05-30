4df6705167911e52f5e9de02f5dd14ae37207130
Andrew TUGMAN <atugman@Andrews-MacBook-Pro.local>
2017-05-30 19:56:56 -0400

    app now updates form based on previous input

--- src/App.js
+++ src/App.js
@@ -9,44 +9,51 @@ class App extends Component {
 
     this.state = {
       movies: [],
-      value: ''
+      userInput: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // componentDidMount() {
-  //
-  //   fetch('https://api.themoviedb.org/3/search/movie?query=%' + this.state.value + '%&api_key=2301535fa250c0bcc1f89c74b2a2a9b4')
-  //     .then(response => response.json())
-  //     .then(movies => {
-  //         this.setState({
-  //           movies
-  //         })
-  //       })
-  //       .catch(err => console.log(err))
-  //     }
-
       handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({userInput: event.target.value});
       }
 
       handleSubmit(event) {
-        // alert('A name was submitted: ' + this.state.value);
-        // componentDidMount() {
-        fetch('https://api.themoviedb.org/3/search/movie?query=%' + this.state.value + '%&api_key=2301535fa250c0bcc1f89c74b2a2a9b4')
+        //const url = 'https://api.themoviedb.org/3/search/movie?query=' + this.state.userInput + '&api_key=2301535fa250c0bcc1f89c74b2a2a9b4'
+        fetch('https://api.themoviedb.org/3/search/movie?query=' + this.state.userInput + '&api_key=2301535fa250c0bcc1f89c74b2a2a9b4')
           .then(response => response.json())
           .then(movies => {
-            console.log('movies ', movies);
+            var input = movies.results[0].title
+            console.log('input for movies with a space ', input);
+            if (input.includes(' ')) {
+              //var removeDigits = /[0-9]/g
+              //var highRegString = input.toUpperCase().replace(removeDigits, '');
+              var splitString = input.split(' ');
+              var lastWord = splitString[splitString.length -1];
+              var firstLetterOfLastWord = lastWord[0];
               this.setState({
                 movies: movies,
                 movieTitle: movies.results[0].title,
                 overview: movies.results[0].overview,
-                backdrop: movies.results[0].backdrop_path,
+                backdrop: 'https://image.tmdb.org/t/p/w500' + movies.results[0].backdrop_path,
                 poster: 'https://image.tmdb.org/t/p/w500' + movies.results[0].poster_path,
-                //value: movies.results[0].title.slice(-1)
+                userInput: firstLetterOfLastWord
               })
+            } else {
+              var input = movies.results[0].title
+              console.log('input ', input)
+              var lastLetterOfWord = input[input.length -1];
+              this.setState({
+                movies: movies,
+                movieTitle: movies.results[0].title,
+                overview: movies.results[0].overview,
+                backdrop: 'https://image.tmdb.org/t/p/w500' + movies.results[0].backdrop_path,
+                poster: 'https://image.tmdb.org/t/p/w500' + movies.results[0].poster_path,
+                userInput: lastLetterOfWord
+              })
+            }
             })
             .catch(err => console.log(err))
           // }
@@ -67,7 +74,7 @@ return (
         <form onSubmit={this.handleSubmit}>
            <label>
              Movie:
-             <input type="text" value={this.state.value} onChange={this.handleChange} />
+             <input type="text" value={this.state.userInput} onChange={this.handleChange} />
            </label>
            <input type="submit" value="Submit" />
          </form>
@@ -75,8 +82,8 @@ return (
          <ul>
            <li>{this.state.movieTitle}</li>
            <li>{this.state.overview}</li>
-           <li>{this.state.backdrop}</li>
            <img className="img-responsive" src={this.state.poster}/>
+           <img className="img-responsive" src={this.state.backdrop}/>
            {/* {this.state.map((movie, index) => (
              <li key={index}>{movie.movies.results[0].title}</li>
            ))} */}
